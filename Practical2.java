271620fab331bb45858d0f5f7f0f41a7a1267173
1U24IT037-GOWTHAMRAJK <gowthamrajk45@gmail.com>
2024-12-11 07:00:53 +0000

    raj2

---
+++ Practical2.java
@@ -0,0 +1,51 @@
+
+class Product 
+{
+    int productId;
+    String productName;
+    double price;
+
+    static int count = 0;
+
+    Product() 
+    {
+        productName = null;
+    }
+
+    Product(int productId, String productName, double price)
+    {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+        count++;
+    }
+
+    double discount(double price, double discountPercentage) 
+    {
+        return ((100 - discountPercentage) / 100) * price;
+    }
+
+    void displayProductInfo()
+    {
+        System.out.println("Product Id : " + productId);
+        System.out.println("Product Name : " + productName);
+        System.out.println("Product Price : " + price);
+    }
+}
+
+public class Practical2 {
+    public static void main(String[] args) {
+        Product product1 = new Product(101, "Laptop", 999.99);
+        Product product2 = new Product(202, "Smart Phone", 499.95);
+        Product product3 = new Product(303, "Tablet", 299.50);
+
+        product1.displayProductInfo();
+        product2.displayProductInfo();
+        product3.displayProductInfo();
+
+        double discountedPrice = product1.discount(product1.price, 10);
+        System.out.println("Discounted Price : " + discountedPrice);
+
+        System.out.println(Product.count);
+    }
+}
